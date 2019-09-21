f12247bc54bc9c9ded10b8681d998688eae1e130
Your Name <farhanscience@hotmail.com>
2019-09-21 10:50:28 +0700

    reverse string es 6

--- javascript-cardio/index.js
+++ javascript-cardio/index.js
@@ -1,8 +1,34 @@
 function reverseString(str) {
- const strArr = str.split('')
- strArr.reverse()
+  // return str
+  //   .split()
+  //   .reverse()
+  //   .join();
+  ///////////////////////////////////////////////////////////
+  // let revString = "";
+  // for (let i = str.length-1; i >= 0; i--) {
+  //   revString = revString + str[i];
+  // }
+  // return revString;
+  // /////////////////////////////////////////////////////
+  // let revString = "";
+  // for (let i = 0; i <= str.length - 1; i++) {
+  //   revString = str[i] + revString
+  // }
+  // return revString;
+  ////////////////////////////////////////////////////////
+  // let revString = "";
+  // for (let char of str) {
+  //   console.log(char);
+  //   revString = char + revString;
+  // }
+  // return revString;
+  //////////////////////////////////////////////////////////
+  // let revString = ''
+  // str.split('').forEach(char => revString = char + revString)
 
- console.log(strArr);
+  // return revString
+  //////////////////////////////////////////////////////////
+  return str.split("").reduce((revString, char) => char + revString, "");
 }
 
-reverseString('hello')
+console.log(reverseString("hello"));
